35dda1d37404678607d5771b06d9f631e6361701
ShayLu <yxlimo@gmail.com>
2020-04-13 16:40:10 +0800

    add test for option: isPublicEndpoint (#2)

--- ocgin/options.go
+++ ocgin/options.go
@@ -8,24 +8,24 @@ import (
 type TraceOption func(o *TraceOptions)
 
 type TraceOptions struct {
-	// DefaultAttributes will be set to each span as default.
-	DefaultAttributes []trace.Attribute
+	// defaultAttributes will be set to each span as default.
+	defaultAttributes []trace.Attribute
 
-	// IsPublicEndpoint should be set to true for publicly accessible HTTP(S)
+	// isPublicEndpoint should be set to true for publicly accessible HTTP(S)
 	// servers. If true, any trace metadata set on the incoming request will
 	// be added as a linked trace instead of being added as a parent of the
 	// current trace.
-	IsPublicEndpoint bool
+	isPublicEndpoint bool
 
-	// Sample is applied to the span started by this Handler around each
+	// sample is applied to the span started by this Handler around each
 	// request. default is 0.0001
-	Sample func(c *gin.Context) trace.Sampler
+	sample func(c *gin.Context) trace.Sampler
 }
 
 var _defaultOptions = TraceOptions{
-	DefaultAttributes: []trace.Attribute{},
-	IsPublicEndpoint:  false,
-	Sample: func(c *gin.Context) trace.Sampler {
+	defaultAttributes: []trace.Attribute{},
+	isPublicEndpoint:  false,
+	sample: func(c *gin.Context) trace.Sampler {
 		return trace.ProbabilitySampler(0.001)
 	},
 }
@@ -33,18 +33,18 @@ var _defaultOptions = TraceOptions{
 // WithDefaultAttributes will be set to each span as default.
 func WithDefaultAttributes(attrs ...trace.Attribute) TraceOption {
 	return func(o *TraceOptions) {
-		o.DefaultAttributes = attrs
+		o.defaultAttributes = attrs
 	}
 }
 
 func WithPublicEndpoint(isPublic bool) TraceOption {
 	return func(o *TraceOptions) {
-		o.IsPublicEndpoint = isPublic
+		o.isPublicEndpoint = isPublic
 	}
 }
 
 func WithSample(f func(c *gin.Context) trace.Sampler) TraceOption {
 	return func(o *TraceOptions) {
-		o.Sample = f
+		o.sample = f
 	}
 }
